5691190f124ff02d33a9239dc5d9908eea6db022
Ashwin Prasad <ashwin@onpar.in>
2022-10-07 10:39:00 +0530

    pls

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  api: {
+    bodyParser: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -8,6 +8,7 @@ type Data = {
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { test } = req.body;
   res.status(200).json({
+    lol: "FUCK",
     data: test,
   });
 }
